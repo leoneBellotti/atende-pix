6140104ea2456985a804621bcc1e8dd676c6acf4
Leone Bellotti Neto <leone.bellotti@processmind.com.br>
2026-05-27 11:31:36 -0300

    chore: configure monorepo workspace

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import vue from 'eslint-plugin-vue';
+
+export default [
+  {
+    ignores: ['dist/**', 'coverage/**', 'node_modules/**']
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...vue.configs['flat/recommended'],
+  {
+    files: ['**/*.vue'],
+    languageOptions: {
+      parserOptions: {
+        parser: tseslint.parser,
+        extraFileExtensions: ['.vue']
+      }
+    }
+  },
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'warn',
+      'vue/multi-word-component-names': 'off'
+    }
+  }
+];
